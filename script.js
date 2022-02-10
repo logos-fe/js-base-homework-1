4a363302b5972eb899c0d11f2a55cdec9d6d290d
NedilskyiMV <nedilskyi.mv@gmail.com>
2022-02-10 22:27:56 +0200

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+// Task 1
+let city = "Kyiv"
+city = "Lviv"
+const address = city
+console.log(address)
+
+// Task 2
+let n = 10
+let result = n**3
+console.log(result)
+
+// Task 3
+let pen = 4
+let pensil = 6
+let penPrice = 5.25
+let pensilPrice = 3.50
+const sum = (pen * penPrice) + (pensil * pensilPrice)
+console.log(sum)
