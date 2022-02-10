86630ecf4ef57648ce3c801dcd7282be97bf6460
oliinyk118 <lezyn1999@gmail.com>
2022-02-10 12:30:27 +0200

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log("hello world")
+
+let city = "Kyiv"
+city = "Lviv"
+const address = city
+
+console.log('city',address)
+
+
+const n = 5
+const result1 =(5**3) //або math pow()
+console.log(result1)
+
+
+const pen = 5.25
+const number_pen = 4
+const pencil = 3.50
+const number_pencil = 6
+
+const result2 = (pen*number_pen+pencil*number_pencil)
+console.log(result2)
+
+
