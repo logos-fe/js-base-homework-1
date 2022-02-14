6aed30d63b520deb08c8ddcf8e1cbd83923e70c6
Andriyana Martsin <andrianamartsin@gmail.com>
2022-02-15 00:20:12 +0200

    dz done

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1
+let city = 'Kyiv';
+city = 'Lviv';
+
+let address = 'Kyiv';
+
+console.log(address);
+
+// 2
+let n = 6;
+const result = Math.pow(n, 3);
+
+console.log(result);
+
+// 3
+const pen = 4;
+const sumPenOne = 5.25;
+
+console.log(pen * sumPenOne);
+
+const pencil = 6;
+const sumPencilOne = 3.5;
+
+console.log(pencil * sumPencilOne);
