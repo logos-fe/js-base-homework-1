34ab033ad9e3999edb3fd785fede2ca51112be83
Bednarska-Iryna <bednarska.ira.2000@gmail.com>
2022-02-14 10:17:15 +0200

    homework 1

---
+++ script.js
@@ -0,0 +1,14 @@
+// task = 1
+let city = "Kyiv"
+city = "Lviv"
+const address = "Lviv"
+console.log(address);
+// task = 2
+const n = 7
+const result = Math.pow(n, 3)
+console.log(result)
+// task = 3
+const pen = 4 * 5.25
+const pencil = 6 * 3.50
+const add = pen + + pencil
+console.log(add)
