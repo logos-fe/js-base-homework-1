b7e379977eca861cf15cd76336f18beaa3d32c3d
unknown <Diana Garbaruk>
2022-02-15 18:40:22 +0200

    Diana Garbaruk

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // #1
 
-let citi = 'Kyiv'
+let city = 'Kyiv'
 citi = 'Lviv'
 
-const address = citi
+const address = city
 console.log("adress", address);
 
 // #2
 
-const n = "5"
+const n = 5
 const result = n ** 3
 console.log("result", result);
 
 // 3#
-const pen = "4"
-const pricePen = '5.25'
+const pen = 4
+const pricePen = 5.25
 
-const pencil = "6"
-const pricePencil = "3.5"
+const pencil = 6
+const pricePencil = 3.5
 
 console.log('sum', pen * pricePen + pencil * pricePencil);
